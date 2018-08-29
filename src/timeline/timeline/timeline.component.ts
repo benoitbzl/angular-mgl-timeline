5b7f60274ee48933d237afde929aa7a7069b6108
Benoit Perrin <benoitbzl@users.noreply.github.com>
2018-08-29 11:36:30 +0200

    Update package version.

--- src/timeline/timeline/timeline.component.ts
+++ src/timeline/timeline/timeline.component.ts
@@ -15,6 +15,9 @@ export class MglTimelineComponent implements AfterViewInit, OnChanges, OnDestroy
   @Input()
   alternate: boolean = true;
 
+  @Input()
+  start: 'left' | 'right' = 'left';
+
   set mobile(value: boolean) {
     this.content && this.content.forEach(entry => entry.mobile = value);
     this.elementRef.nativeElement.classList.toggle('mobile', value)
@@ -71,7 +74,7 @@ export class MglTimelineComponent implements AfterViewInit, OnChanges, OnDestroy
               }
             }));
         }
-        entry.alternate = this.alternate ? index % 2 !== 0 : false;
+        entry.alternate = this.alternate ? index % 2 !== (this.start === 'left' ? 0 : 1) : false;
         entry.mobile = this.mobile;
         entry.focusOnOpen = this.focusOnOpen;
       });
